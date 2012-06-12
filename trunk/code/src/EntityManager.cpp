a289f483c0fbca9bc1969d4a874b87a66c71639e
roblach9999@gmail.com <roblach9999@gmail.com@126fa39a-4a23-d5ba-52e0-8d1ccc232c09>
2012-06-12 02:12:42 +0000

    Forgot to push these (EntityManager.cpp)

---
+++ trunk/code/src/EntityManager.cpp
@@ -0,0 +1,82 @@
+#include "EntityManager.h"
+#include <algorithm>
+
+EntityManager::EntityManager()
+{
+   playerNames.reserve(sizeof(string)*64);
+}
+
+void EntityManager::Update(sf::Time elapsed)
+{
+    //Update Players
+    for(vector<string>::iterator iter = playerNames.begin(); iter != playerNames.end(); iter++){
+        players[*iter]->Update(elapsed);
+    }
+
+    //Update Entities
+    for(vector<Entity*>::iterator iter = ents.begin(); iter != ents.end(); iter++){
+        (*iter)->Update(elapsed);
+    }
+}
+
+void EntityManager::Render(Camera* activeCamera)
+{
+
+    //Update Players
+    for(vector<std::string>::iterator iter = playerNames.begin(); iter != playerNames.end(); iter++){
+        players[*iter]->Render(activeCamera);
+    }
+
+    //Update Entities
+    for(vector<Entity*>::iterator iter = ents.begin(); iter != ents.end(); iter++){
+        (*iter)->Render(activeCamera);
+    }
+
+
+}
+
+//Players
+void EntityManager::AddPlayer(string name, Entity* player)
+{
+    playerNames.insert(playerNames.begin(), name);
+    players.insert(pair<std::string, Entity*>(name, player));
+
+#ifdef DEBUG
+    cout<<"Added Player name: "<<name
+    <<" id: "<<player->getID()
+    <<" position: "<<player->getPosition().x<<" "<<player->getPosition().y<<" "<<player->getPosition().z<<"\n";
+    cout<<"Current Players: ";
+    for(vector<string>::iterator iter = playerNames.begin(); iter != playerNames.end(); iter++){
+        cout<<players[*iter]->getID()<<" ";
+    }
+    cout<<"\n";
+#endif
+}
+
+void EntityManager::RemovePlayer(string name)
+{
+   players.erase(name);
+   playerNames.erase(remove(playerNames.begin(), playerNames.end(), name), playerNames.end());
+}
+
+Entity* EntityManager::GetPlayer(std::string name)
+{
+    return players[name];
+
+}
+
+//Local Entities
+void EntityManager::AddEntity(Entity* entity)
+{
+    ents.insert(ents.begin(), entity);
+}
+
+void EntityManager::RemoveEntity(Entity* entity)
+{
+    ents.erase(std::remove(ents.begin(),ents.end(), entity), ents.end());
+}
+
+EntityManager::~EntityManager()
+{
+
+}
